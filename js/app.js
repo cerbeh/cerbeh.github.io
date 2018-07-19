3f0532ccf6ba60470d623aaa8e2fc852e28d8ada
Martin Allgood <martintallgood@gmail.com>
2018-07-19 12:15:16 +0100

    Adjusted modals to now have the html injected in to them using jquery rather then hardcoded on index.html

--- js/app.js
+++ js/app.js
@@ -1,7 +1,45 @@
 $(()=>{
   const $aboutButton = $('#about-me-button');
+  const $projectsButton = $('#projects-button');
+  const $skillsButton = $('#skills-button');
+  const $contactButton = $('#contact-button');
+
+  const $modal = $('#modal');
+  const $modalBackground = $('.modal-background');
+  const $modalContent = $('.modal-content');
+
+  const modalContent = {
+    aboutPage:
+    `<figure class="image is-3by4">
+        <img src="./assets/martin-cropped.jpg" alt="Picture of Martin Allgood">
+    </figure>`,
+    projectsPage:
+    `<figure class="image is-3by4">
+      <img src="./assets/martin-2.jpg" alt="Picture of Martin Allgood">
+    </figure>`
+  };
 
   $aboutButton.on('click', () => {
-    console.log('clicked');
+    $modalContent.html(modalContent.aboutPage);
+    $modal.toggleClass('is-active');
   });
+
+  $projectsButton.on('click', () => {
+    $modalContent.html(modalContent.projectsPage);
+    $modal.toggleClass('is-active');
+  });
+
+  $skillsButton.on('click', () => {
+    $modal.toggleClass('is-active');
+  });
+
+  $contactButton.on('click', () => {
+    $modal.toggleClass('is-active');
+  });
+
+
+  $modalBackground.on('click', () => {
+    $modal.toggleClass('is-active');
+  });
+
 });
