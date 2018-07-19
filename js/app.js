e4dca8f04219b6fc4146e95ce6418165ef2de751
Martin Allgood <martintallgood@gmail.com>
2018-07-19 15:13:45 +0100

    hero-footer now toggles the classes and each one now opens up unique modal with content defined in modalContent object

--- js/app.js
+++ js/app.js
@@ -36,32 +36,39 @@ const modalContent = {
   </div>
   <figure class="image is-3by4">
     <img src="./assets/martin-2.jpg" alt="Picture of Martin Allgood">
-  </figure>`
+  </figure>`,
 
+  skillsPage: '<h1 class="title is-3">Skills</h1>',
+  contactPage:
+  `Find me on:
+  <ul>
+    <li>linkedIn</li>
+    <li>
+      <a class="button is-large">
+        <span class="icon is-medium">
+          <i class="fab fa-github"></i>
+        </span>
+        <span>GitHub</span>
+      </a>
+    </li>
+  </ul>`
 };
 
 $(()=>{
-  
+
   const $buttons = $('.button-style');
 
   const $modal = $('#modal');
   const $modalClose = $('.close-modal');
   const $modalContent = $('.modal-content');
 
-  $buttons.on('click', ({ currentTarget: { id } }) => {
+  $buttons.on('click', ({ target: { id }}) => {
     $modalContent.html(modalContent[id]);
     $modal.toggleClass('is-active');
   });
 
   $modalClose.on('click', () => {
     $modal.toggleClass('is-active');
-  });
-
-
-  //Do i have a navburger at the moment?
-  $('.navbar-burger').click(function() {
-    $('.navbar-burger').toggleClass('is-active');
-    $('.navbar-menu').toggleClass('is-active');
   });
 
 });
