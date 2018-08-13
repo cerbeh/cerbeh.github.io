70f71d66fc98810c729083ad7903b6af44e20a66
Martin Allgood <martintallgood@gmail.com>
2018-08-13 14:14:29 +0100

    Split out the modal SCSS into seperate file

--- js/app.js
+++ js/app.js
@@ -4,15 +4,18 @@ $(()=>{
   const $modal = $('#modal');
   const $modalClose = $('.close-modal');
   const $modalContent = $('.modal-content');
+  const $main = $('#main');
 
   $buttons.on('click', ({ target: { id }}) => {
     $modalContent.load(`/assets/${id}.html`, () =>  {
       $modal.toggleClass('is-active');
+      // $main.toggleClass('animate-out');
     });
   });
 
   $modalClose.on('click', () => {
     $modal.toggleClass('is-active');
+    // $main.toggleClass('animate-out');
   });
 
 });
