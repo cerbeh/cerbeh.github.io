afc00ebe7d739f3c96c8770e9ee11c4d4cc67ef7
Martin Allgood <martintallgood@gmail.com>
2018-08-14 11:03:36 +0100

    Added animations

--- js/app.js
+++ js/app.js
@@ -4,18 +4,32 @@ $(()=>{
   const $modal = $('#modal');
   const $modalClose = $('.close-modal');
   const $modalContent = $('.modal-content');
-  const $main = $('#main');
+  // const $main = $('#main');
 
   $buttons.on('click', ({ target: { id }}) => {
     $modalContent.load(`/assets/${id}.html`, () =>  {
+      // $modal.removeAttr('class').addClass('content-enter');
       $modal.toggleClass('is-active');
       // $main.toggleClass('animate-out');
     });
   });
 
   $modalClose.on('click', () => {
+    $modal.addClass('out');
     $modal.toggleClass('is-active');
     // $main.toggleClass('animate-out');
   });
 
 });
+
+//
+// $('.button').click(function(){
+//   var buttonId = $(this).attr('id');
+//   $('#modal-container').removeAttr('class').addClass(buttonId);
+//   $('body').addClass('modal-active');
+// })
+//
+// $('#modal-container').click(function(){
+//   $(this).addClass('out');
+//   $('body').removeClass('modal-active');
+// });
