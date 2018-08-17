a56ac2a65fc9355425bc6f5d8977c1abb09c4aa1
Martin Allgood <martintallgood@gmail.com>
2018-08-18 00:57:17 +0100

    Experimented with storing some of the element variables and on.click events inside a single object

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 $(()=>{
 
-  const $buttons = $('.button-style');
+  // const $buttons = $('.button-style');
   const $modal = $('#modal');
   const $modalClose = $('.close-modal');
   const $modalContent = $('.modal-content');
 
   const clickEvent = {
+
+    elements: {
+      $buttons: () => $('.button-style')
+    },
 
     openTab: ({target: { id }}) => {
       $modalContent.load(`/assets/${id}.html`, () =>  {
@@ -20,7 +24,7 @@ $(()=>{
       $(`#${$currentButton.id}`).addClass('is-active');
     },
 
-    navigateProjects: ( {currentTarget: { attributes: { navigateTo } }} ) => {
+    navigateProjects: ({currentTarget: { attributes: { navigateTo }}}) => {
       const projectName = navigateTo.value;
       $('#project-content').load(`/assets/${projectName}.html`);
       $('.projects-buttons').parent().removeAttr('class');
@@ -30,16 +34,22 @@ $(()=>{
     closeModal: () => {
       $modal.addClass('out');
       $modal.toggleClass('is-active');
+    },
+
+    buttonClick: () => {
+      return clickEvent.elements.$buttons().on('click', clickEvent.openTab);
     }
 
   };
 
-  $buttons.on('click', clickEvent.openTab);
+  // $buttons.on('click', clickEvent.openTab);
 
   $modal.on('click', '.projects-buttons', clickEvent.chooseProjectTab);
   $modal.on('click', '.next-buttons', clickEvent.navigateProjects);
 
   $modalClose.on('click', clickEvent.closeModal);
+
+  clickEvent.buttonClick();
 
 });
 
