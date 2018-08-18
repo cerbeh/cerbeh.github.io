7dc76fbba330f225048f7d44ec8fbb80e3b0655f
Martin Allgood <martintallgood@gmail.com>
2018-08-18 12:04:37 +0100

    Renamed function object 'userAction' and finished refactoring the tab toggle functions

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
 $(()=>{
 
-  // const $buttons = $('.button-style');
+  const $buttons = $('.button-style');
   const $modal = $('#modal');
   const $modalClose = $('.close-modal');
   const $modalContent = $('.modal-content');
 
-  const clickEvent = {
-
-    elements: {
-      $buttons: () => $('.button-style')
-    },
+  const userAction = {
 
     openTab: ({target: { id }}) => {
       $modalContent.load(`/assets/${id}.html`, () =>  {
@@ -18,15 +14,13 @@ $(()=>{
     },
 
     chooseProjectTab: ({currentTarget: { parentElement }}) => {
-      const $currentButton = parentElement;
-      $('#project-content').load(`/assets/${$currentButton.id}.html`);
-      $('.projects-buttons').parent().removeAttr('class');
-      $(`#${$currentButton.id}`).addClass('is-active');
+      const currentButton = parentElement;
+      userAction.toggleProjectTabs(currentButton);
     },
 
     navigateProjects: ({currentTarget: { attributes: { navigateTo }}}) => {
       const projectName = navigateTo.value;
-      clickEvent.toggleProjectTabs(projectName);
+      userAction.toggleProjectTabs(projectName);
     },
 
     toggleProjectTabs: (project) => {
@@ -38,22 +32,17 @@ $(()=>{
     closeModal: () => {
       $modal.addClass('out');
       $modal.toggleClass('is-active');
-    },
-
-    buttonClick: () => {
-      return clickEvent.elements.$buttons().on('click', clickEvent.openTab);
     }
 
   };
 
-  // $buttons.on('click', clickEvent.openTab);
+  $buttons.on('click', userAction.openTab);
 
-  $modal.on('click', '.projects-buttons', clickEvent.chooseProjectTab);
-  $modal.on('click', '.next-buttons', clickEvent.navigateProjects);
+  $modal.on('click', '.projects-buttons', userAction.chooseProjectTab);
+  $modal.on('click', '.next-buttons', userAction.navigateProjects);
 
-  $modalClose.on('click', clickEvent.closeModal);
+  $modalClose.on('click', userAction.closeModal);
 
-  clickEvent.buttonClick();
 
 });
 
